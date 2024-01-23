96ff8442930b99b0009db389c2d70e4da833f5a6
jeongjjy <wodud4611@naver.com>
2024-01-23 11:48:07 +0900

    커밋

--- src/main/java/com/ict/project/control/CourseController.java
+++ src/main/java/com/ict/project/control/CourseController.java
@@ -24,7 +24,7 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class CourseController {
-     @Autowired
+    @Autowired
     HttpServletRequest request;
     @Autowired
     HttpSession session;
