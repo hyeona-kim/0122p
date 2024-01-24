0146f63077794a8a0bb96f7a111e9a29a1e72fbc
sangkiYoon <dbstkdrl123@naver.com>
2024-01-24 18:55:21 +0900

    si

--- src/main/java/com/ict/project/vo/CounselAddVO.java
+++ src/main/java/com/ict/project/vo/CounselAddVO.java
@@ -9,4 +9,6 @@ public class CounselAddVO {
   
     private String ss_idx, sf_idx, ss_time, so_idx, tr_idx, ss_msg, ss_msubject, ss_bd, ss_member, ss_mday, ss_pa,
     ss_cnt,ss_day, c_idx;
+
+    private CounselVO co_vo;
 }
