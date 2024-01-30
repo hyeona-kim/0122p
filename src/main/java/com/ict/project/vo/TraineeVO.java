4cbe9d147ab0fc23033183ba35c1eb151b3ede45
lkeeeeeee <lke829@naver.com>
2024-01-30 17:50:48 +0900

    dd

--- src/main/java/com/ict/project/vo/TraineeVO.java
+++ src/main/java/com/ict/project/vo/TraineeVO.java
@@ -11,7 +11,7 @@ public class TraineeVO {
 	  ct_code, tr_total_fee,tr_nowstatus;*/
 
 //선우씨, 현아씨 합의
-	private String tr_idx, tr_name, tr_rrn, tr_phone, tr_hp, tr_pos_code, tr_addr, status, memo, c_idx, ct_code, tr_total_fee, ss_end, c_img, tr_nowstatus, ss_num,s_code;
+	private String tr_idx, tr_name, tr_rrn, tr_phone, tr_hp, tr_pos_code, tr_addr, status, memo, c_idx, ct_code, tr_total_fee, ss_end, c_img, tr_nowstatus, ss_num,s_code,tr_card,tr_cancel_date,tr_cancel_text,tr_hrd;
 
 	private CourseVO cvo;
 }
