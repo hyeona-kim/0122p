53c488d0569662c8393da8453c7c482d452274bd
tanksoda <aaadddo@naver.com>
2024-02-27 20:10:47 +0900

    afafafaf

--- src/main/java/com/ict/project/vo/EvaluationStatusVO.java
+++ src/main/java/com/ict/project/vo/EvaluationStatusVO.java
@@ -9,4 +9,5 @@ public class EvaluationStatusVO {
     private String es_idx, es_name, es_type, es_examStatus, es_gradingStatus,
             sf_idx, es_date, es_time, es_totalScore, es_level, es_num_question,
             sk_idx, s_idx, es_status;
+    private QuestionVO[] qt_ar;
 }
