36c8c27680247101fcbefbf9fc674017f962c223
lkeeeeeee <lke829@naver.com>
2024-02-28 18:29:36 +0900

    주별시간표 마무리

--- src/main/java/com/ict/project/vo/TimeVO.java
+++ src/main/java/com/ict/project/vo/TimeVO.java
@@ -7,7 +7,8 @@ import lombok.Setter;
 @Setter
 public class TimeVO {
 	// 훈련일자.,훈련시작시간,훈련종료시간,방학여부,시작시간,시간구분,훈련강사코드,교육장소코드,교과목능력단위코드
-	private String training_date, training_starttime, training_endtime, status, start_time, time_split, r_name, s_name,
+	private String training_date, training_starttime, training_endtime, status, start_time, time_split, sf_name, r_name,
+			s_name,
 			week;
 
 }
